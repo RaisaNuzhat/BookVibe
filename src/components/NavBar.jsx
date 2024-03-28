74a14f890ff4b4fcc9ef0690ff50e692d7195b3d
RaisaNuzhat <raisa.csecu@gmail.com>
2024-03-28 21:31:34 +0600

    responsive done

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,13 +4,13 @@ const NavBar = () => {
     return (
         <div className="navbar bg-base-100 fixed shadow-lg px-4 container mx-auto z-10">
         <div className="flex-1">
-          <Link to='/' className="btn btn-ghost  gap-0 text-3xl normal-case text-[#131313]">Book Vibe</Link>
+          <Link to='/' className="btn btn-ghost text-[18px] gap-0 lg:text-3xl normal-case text-[#131313]">Book Vibe</Link>
         </div>
         <div className="flex-none gap-2">
-          <ul className="menu menu-horizontal px-1 text-xl hidden sm:flex gap-5 items-center">
-            <NavLink to='/' className={({isActive})=>isActive  ? 'text-[#23BE0A] font-bold ':' text-[#131313] font-bold'}>Home</NavLink>
-            <NavLink to='/listedbooks' className={({isActive})=>isActive  ? 'text-[#23BE0A] font-bold ':' text-[#131313] font-bold'}>Listed Books</NavLink>
-            <NavLink to='/pagestoread' className={({isActive})=>isActive  ? 'text-[#23BE0A] font-bold':' text-[#131313] font-bold'}>Pages to Read</NavLink>
+          <ul className="menu menu-horizontal px-1 lg:text-xl text-[12px] sm:flex gap-5 items-center">
+            <NavLink to='/' className={({isActive})=>isActive  ? 'text-[#23BE0A] lg:font-bold  ':' text-[#131313] lg:font-bold '}>Home</NavLink>
+            <NavLink to='/listedbooks' className={({isActive})=>isActive  ? 'text-[#23BE0A] lg:font-bold  ':' text-[#131313] lg:font-bold '}>Listed Books</NavLink>
+            <NavLink to='/pagestoread' className={({isActive})=>isActive  ? 'text-[#23BE0A] lg:font-bold ':' text-[#131313] lg:font-bold '}>Pages to Read</NavLink>
             <button className="btn bg-[#23BE0A] text-white">Sign In</button>
             <button className="btn  bg-[#59C6D2] text-white">Sign Up</button>
 
